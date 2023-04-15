9a4c57337bb6aa72c8488097ecdb1607d68b9c8b
dell-AmrSayed <amrasayed15@hotmail.com>
2023-04-15 15:20:32 +0200

    recursive function first commit

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,46 +1,32 @@
-﻿internal class Program
+﻿
+
+internal class Program
 {
-    static void Main()
-
+  
+    static Tuple<long, int, int> sumRecursiveFn(int[,] matrix, int row = 0, long bestSum = long.MinValue, int bestRow = 0, int bestCol = 0)
     {
-
-        // Declare and initialize the matrix
-
-        int[,] matrix = {
-
-                   { 0, 2, 4, 0, 9, 5 },
-
-                   { 7, 1, 3, 3, 2, 1 },
-
-                   { 1, 3, 9, 8, 5, 6 },
-
-                   { 4, 6, 7, 9, 1, 0 }
-
-        };
-
-
-
-        // Find the maximal sum platform of size 2 x 2
-
-        long bestSum = long.MinValue;
-
-        int bestRow = 0;
-
-        int bestCol = 0;
-
-
-
-        for (int row = 0; row < matrix.GetLength(0) - 2; row++)
-
+        if (row < matrix.GetLength(0) - 2)
         {
-
             for (int col = 0; col < matrix.GetLength(1) - 2; col++)
 
             {
+                //the summation row and column here will be the center of the matrix 
 
-                long sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
-                    matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
-                     matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+               
+                    long sum = matrix[row, col];
+                    sum += matrix[row, col + 1];
+                    sum += matrix[row, col + 2];
+
+
+                    sum += matrix[row + 1, col ];
+                    sum += matrix[row + 1, col + 1];
+                    sum += matrix[row + 1, col + 2];
+
+                    sum += matrix[row + 2, col];
+                    sum += matrix[row + 2, col + 1];
+                    sum += matrix[row + 2, col + 2];
+                
+
 
                 if (sum > bestSum)
 
@@ -55,34 +41,86 @@
                 }
 
             }
+            Console.WriteLine(bestSum);
+            return sumRecursiveFn(matrix, row + 1, bestSum, bestRow, bestCol);
+        }
+        else
+        {
+            return Tuple.Create(bestSum, bestRow, bestCol);
+        }
+
+
+    }
+
+  
+    static void Main()
+
+    {
+        int test = Convert.ToInt32(Console.ReadLine());//number of rows
+        Console.WriteLine(test);
+
+        int m, n;
+
+        // Declare and initialize the matrix
+        Console.WriteLine("please enter the number of rows needed?");
+        m = Convert.ToInt32(Console.ReadLine());//number of rows
+        Console.WriteLine("please enter the number of columns needed?");
+        n = Convert.ToInt32(Console.ReadLine());//number of columns
+        int[,] matrix = new int[m, n];
+        for (int i = 0; i < m; i++) {//loop to read the values which will be stored in the array 
+
+            for (int j = 0; j < n; j++) {
+                Console.WriteLine("please enter the value needed fo row:" + i + ", column j:" + j + " ?");
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {//loop to PRINT MATRIX
+            Console.Write("[");
+
+            for (int j = 0; j < n; j++)
+            { if (j == n - 1)
+                { Console.Write(matrix[i, j]); }
+                else
+                { Console.Write(matrix[i, j] + ","); }
+
+
+            }
+            Console.Write("]\n");
 
         }
 
 
 
+
+        // Find the maximal sum platform of size 2 x 2
+
+
+
+
+
+        Tuple<long, int, int> getvalues = sumRecursiveFn(matrix);
+
+
+  
         // Print the result
 
-        Console.WriteLine("The best platform is:");
+        Console.WriteLine("The best sumation is:"+ getvalues.Item1);
+        Console.WriteLine("The best row is:"+ getvalues.Item2+ "row is:"+ getvalues.Item3);
 
-        Console.WriteLine("  {0} {1} {2}",
+        for (int i = getvalues.Item2; i < getvalues.Item2 + 3; i++)
+        {//loop to read the values which will be stored in the array 
 
-                   matrix[bestRow, bestCol],
-                   matrix[bestRow, bestCol + 1],
-                   matrix[bestRow, bestCol + 2]);
+            for (int j = getvalues.Item3; j < getvalues.Item3+3; j++)
+            {
+                Console.WriteLine("please enter the value needed fo row:" + i + ", column j:" + j + " ?");
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("  {0} {1} {2}",
+            }
+        }
 
-              matrix[bestRow + 1, bestCol],
-              matrix[bestRow + 1, bestCol + 1],
-              matrix[bestRow + 1, bestCol + 2]);
-
-
-        Console.WriteLine("  {0} {1} {2}",
-
-              matrix[bestRow + 2, bestCol],
-              matrix[bestRow + 2, bestCol + 1],
-              matrix[bestRow + 2, bestCol + 2]);
-        Console.WriteLine("The maximal sum is: {0}", bestSum);
 
     }
 }
